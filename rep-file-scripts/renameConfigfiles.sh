425694df0fb183679d23dc56d3ed0a276136b971
acwinblad <acwinblad@gmail.com>
2018-09-06 11:41:58 -0600

    Added rep script files

---
+++ rep-file-scripts/renameConfigfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# this is how to rename the files quickly
+#rename 's/^/template\-/' '*.py'
+#rename 's/\*.py//\' '*'
+#echo $(rename 's/^/template\-/' *.py)
+#echo $(rename 's/\*.py//\' *)
